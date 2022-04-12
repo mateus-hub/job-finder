16eb1af89335263cbd3c9f4d0629ff70e1124f47
mateus-hub <theusskt2018@gmail.com>
2022-04-11 21:08:22 -0300

    Creating route to add records

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const db = require("./db/connection");
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
 
 app.listen(PORT, function () {
   console.log(`O express está rodando na porta ${PORT}`);
 });
+
+// body parser
+app.use(bodyParser.urlencoded({ extend: false }));
 
 // db connection
 db.authenticate()
@@ -21,3 +25,6 @@ db.authenticate()
 app.get("/", (req, res) => {
   res.send("Está funcionando 3");
 });
+
+// jobs routes
+app.use("/jobs", require("./routes/jobs"));
--- models/Job.js
+++ models/Job.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const db = require(".../db/connection");
+const db = require("../db/connection");
 
 const Job = db.define("job", {
   title: {
